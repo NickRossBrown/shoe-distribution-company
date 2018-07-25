5d6554c86a21e793650513e758b1e474c35ff73e
Nick Brown <nickrossbrown@gmail.com>
2018-07-24 19:36:24 -0700

    add two passing brand_spec tests for brand name and brand length

--- lib/brand.rb
+++ lib/brand.rb
@@ -2,4 +2,6 @@ class Brand < ActiveRecord::Base
    has_and_belongs_to_many(:shoes)
    has_many :inventorys
    has_many :stores, through: :inventorys
+   validates(:name, :presence => true)
+   validates(:name, {:presence => true, :length => { :maximum => 50 }})
 end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,11 +1,22 @@
 require("spec_helper")
 
-
 describe(Brand) do
   describe('brand.name and brand.price') do
     it('will create a new brand with a name and price') do
       brand = Brand.create({:name => "Huggs", :price => 35})
-      expect([brand.name,brand.price]).to(eq(["Slippers",35]))
+      expect([brand.name,brand.price]).to(eq(["Huggs",35]))
     end
   end
+
+
+  it("validates presence of the brand name") do
+    brand= Brand.new({:name => ""})
+    expect(brand.save()).to(eq(false))
+  end
+
+
+  it("ensures the length of name is at most 50 characters") do
+  brand= Brand.new({:name => "a".*(51)})
+  expect(brand.save()).to(eq(false))
+end
 end
